7259deb8f82f1130cd84e913d84cd8228b7029e2
serhatxbulut <serhatbulut1967@gmail.com>
2026-01-11 19:25:13 +0300

    Yemek.kt

---
+++ Yemek.kt
@@ -0,0 +1,10 @@
+package com.fittrack.app.model
+
+data class Yemek(
+    val id: Int,
+    val ad: String,
+    val kalori: Int,
+    val protein: Double,
+    val karbonhidrat: Double,
+    val yag: Double
+)
